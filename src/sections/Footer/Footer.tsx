727a3619e5d01d4ab943e23788742789f6ae59dd
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-08-31 14:45:41 +0800

    Add footer

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,5 +1,58 @@
+import facebookIcon from '../../assets/logos/facebook.svg';
+import linkedinIcon from '../../assets/logos/linkedin.svg';
+import githubIcon from '../../assets/logos/github.svg';
+import whiteLogo from '../../assets/logos/white-logo.svg';
+
 const Footer = (): JSX.Element => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h4>SITEMAP</h4>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Get started</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>RESOURCES</h4>
+          <ul>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>FOLLOW US</h4>
+          <ul className="social-icons">
+            <li>
+              <a href="#">
+                <img src={facebookIcon} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={linkedinIcon} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/nknguyenhc/CS3216-A1">
+                <img src={githubIcon} alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-logo">
+        <img src={whiteLogo} alt="InternTwine Logo" />
+        <span>InternTwine</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
